a0eb53482fbdf7d21472c13f07d6804fceb3f66d
mehran <mehransheyhki11@yahoo.com>
2023-06-19 11:29:09 +0330

    did part of discount

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-console.log("سیمرغ");
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+let $ = document;
+let navbarElem = $.getElementById('navbar'); 
+console.log(navbarElem);
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if ($.body.scrollTop > 50 || $.documentElement.scrollTop > 50) {
+    navbarElem.classList.add('shadow-sm');
+  } else {
+    navbarElem.classList.remove('shadow-sm');
+  }
+}
