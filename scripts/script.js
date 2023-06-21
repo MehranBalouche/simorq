fee84588f005c0ab6b4a0ad14ea6f60c0944768d
mehran <mehransheyhki11@yahoo.com>
2023-06-22 01:35:38 +0330

    first commit8

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,33 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 let $ = document;
 let navbarElem = $.getElementById('navbar'); 
-console.log(navbarElem);
+let moveTopBtnElem = $.getElementById('moveTopBtn');
+console.log(moveTopBtnElem);
+
+moveTopBtnElem.addEventListener('click', backToTop);
+
 window.onscroll = function () {
   scrollFunction();
 };
+
+
 function scrollFunction() {
   if ($.body.scrollTop > 50 || $.documentElement.scrollTop > 50) {
     navbarElem.classList.add('shadow-sm');
   } else {
     navbarElem.classList.remove('shadow-sm');
   }
+
+  if ($.body.scrollTop > 250 || $.documentElement.scrollTop > 250){
+    moveTopBtnElem.style.display = 'block';
+  }else{
+    moveTopBtnElem.style.display = 'none';
+
+  }
+}
+
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
